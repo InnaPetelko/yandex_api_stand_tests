20f953a50b9398be0d83a5965d0c465e8753a8df
InnaPetelko <inna-petelko@yandex.ru>
2026-05-06 09:59:33 +0300

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://b0b4c719-6842-4366-a2fb-9be6d2dfde27.serverhub.praktikum-services.ru"
+
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,11 @@
+order_body = {
+    "firstName": "Vera",
+    "lastName": "Levina",
+    "address": "Nagornaya",
+    "metroStation": "17",
+    "phone": "+7 929 111 22 33",
+    "rentTime": 5,
+    "deliveryDate": "2026-05-04",
+    "comment": "Не звонить",
+    "color": ["BLACK"]
+}
